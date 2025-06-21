0de926d052bf20536597b775b32267d64cad465b
Rohan Shah <70683368+AfkRohan@users.noreply.github.com>
2025-06-21 18:23:10 -0400

    frontend

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  allowedHosts: ['*'],
   plugins: [react()],
+   server: {
+    allowedHosts: [
+      '.sidepro.app', // allow all SidePro subdomains
+      'numerologeraifront-jun21-617pm-00s-rohan-shah-beta.platform.beta.sidepro.app'
+    ],
+    host: '0.0.0.0' // optional: ensures Vite listens on all interfaces
+  }
 })
