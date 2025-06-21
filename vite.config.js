f90a01de021a818472d1a10961448426fc893ecb
Rohan Shah <70683368+AfkRohan@users.noreply.github.com>
2025-06-21 18:07:57 -0400

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  allowedHosts: ['.sidepro.app']
+  allowedHosts: ['.sidepro.app'],
   plugins: [react()],
 })
